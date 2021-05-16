dbe0baf57170b5b330f93227f6f6d475735d48e2
cellulosami <ajshields196@gmail.com>
2021-05-16 15:30:27 -0400

    make cube rotate through colors

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -17,20 +17,23 @@ public class Cube : MonoBehaviour
     public float scaleMagnitude;
     private float scaleTime;
 
+    //color variables
+    private string currentColor = "red";
+    private float blue = 1.0f;
+    private float green = 0.0f;
+    private float red = 0.0f;
+    public float colorSpeed;
+
+
     void Start()
     {
-        //setup orbit variables
-        // orbitSpeed = 1;
-        // orbitWidth = 1;
-        // orbitHeight = 1;
-
 
         transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(0.5f, 255.0f, 0.3f, 0.4f);
     }
     
     void Update()
@@ -38,7 +41,7 @@ public class Cube : MonoBehaviour
         scaleTime += Time.deltaTime * scaleSpeed;
         orbitTime += Time.deltaTime * orbitSpeed;
 
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(-50.0f * Time.deltaTime, -50.0f * Time.deltaTime, 0.0f);
 
         //orbit cube
         float x = Mathf.Cos(orbitTime) * orbitWidth;
@@ -48,5 +51,36 @@ public class Cube : MonoBehaviour
         //scale cube
         float currentScale = (Mathf.Sin(scaleTime)) + 1;
         transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+
+        //color cube
+        if (currentColor == "red" && blue <= 0.0f) {
+            currentColor = "green";
+        } else if (currentColor == "green" && red <= 0.0f) {
+            currentColor = "blue";
+        } else if (currentColor =="blue" && green <= 0.0f) {
+            currentColor = "red";
+        }
+        
+        if (currentColor == "red") {
+            if (red <= 1.0f) {
+                red += 0.01f * colorSpeed;
+            } else {
+                blue -= 0.01f * colorSpeed;
+            }
+        } else if (currentColor == "green") {
+            if (green <= 1.0f) {
+                green += 0.01f * colorSpeed;
+            } else {
+                red -= 0.01f * colorSpeed;
+            }
+        } else if (currentColor == "blue") {
+            if (blue <= 1.0f) {
+                blue += 0.01f * colorSpeed;
+            } else {
+                green -= 0.01f * colorSpeed;
+            }
+        }
+
+        Renderer.material.color = new Color(red, green, blue, 1);
     }
 }
